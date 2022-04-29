478c9fd9b6577fb61f7d2680b9d36dde9b4042fa
Jochen Just <jochen.just@novatec-gmbh.de>
2022-04-29 14:30:52 +0200

    Restructure code

--- goroutine-downloader.go
+++ goroutine-downloader.go
@@ -8,38 +8,6 @@ import (
     "path/filepath"
 )
 
-type readmeStats struct {
-    name string
-    format string
-    size int64
-    url string
-}
-
-type format struct {
-    name string
-    fileEnding string
-}
-
-
-func check(e error) {
-    if e != nil {
-        panic(e)
-    }
-}
-
-func writeToFile(file *os.File, content io.Reader) {
-    fmt.Println("Writing to file:", file.Name())
-    _, err := io.Copy(file, content)
-    check(err)
-}
-
-func downloadReadMe(repoName string) io.ReadCloser {
-    repoUrl := fmt.Sprintf("https://raw.githubusercontent.com/NovatecConsulting/%v/master/README.md", repoName)
-    resp, err := http.Get(repoUrl)
-    check(err)
-
-    return resp.Body
-}
 
 func checkRepoFor(repoName string, branchName string, format *format) *readmeStats {
     repoUrl := fmt.Sprintf("https://raw.githubusercontent.com/NovatecConsulting/%v/%v/README.%v", repoName, branchName, format.fileEnding)
@@ -113,4 +81,36 @@ func main_download() {
         defer content.Close()
         writeToFile(file, content)
     }
+}
+type readmeStats struct {
+    name string
+    format string
+    size int64
+    url string
+}
+
+type format struct {
+    name string
+    fileEnding string
+}
+
+
+func check(e error) {
+    if e != nil {
+        os.Exit(1)
+    }
+}
+
+func writeToFile(file *os.File, content io.Reader) {
+    fmt.Println("Writing to file:", file.Name())
+    _, err := io.Copy(file, content)
+    check(err)
+}
+
+func downloadReadMe(repoName string) io.ReadCloser {
+    repoUrl := fmt.Sprintf("https://raw.githubusercontent.com/NovatecConsulting/%v/master/README.md", repoName)
+    resp, err := http.Get(repoUrl)
+    check(err)
+
+    return resp.Body
 }
